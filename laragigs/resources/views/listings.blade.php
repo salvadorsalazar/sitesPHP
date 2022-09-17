4657cda24f4963d709ad3e5bae96e142df77481d
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-09-17 00:12:30 -0500

    added partials view

--- laragigs/resources/views/listings.blade.php
+++ laragigs/resources/views/listings.blade.php
@@ -1,7 +1,7 @@
 <!-- <h1>Listings</h1> -->
 @extends('layout')
 @section('content')
-
+@include('partials._hero')
 <div
                 class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
             >
