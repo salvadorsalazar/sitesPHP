4faa066ac498b10bd3b0ce92c2fae65e1e475b33
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-09-21 22:52:45 -0500

    linked contollers

--- laragigs/app/Http/Controllers/ListingController.php
+++ laragigs/app/Http/Controllers/ListingController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    //
+    //show all listings
+    public function index(){
+return view('listings',[
+        
+        'listings' => Listing::all()
+          ]);
+    }
+
+    //showsingle listing
+    public function show(Listing $listing){
+       return view('listing',[
+        'listing' => $listing
+    ]); 
+    }
+
+
 }
--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -14,28 +15,11 @@ use App\Models\Listing;
 |
 */
 //all listings
-Route::get('/', function () {
-    return view('listings',[
-        'heading' => 'lateset listings',
-        'listings' => Listing::all()
-          ]);
-
-
-
-
-});
+Route::get('/', [ListingController::class,'index']);
         
   
           //single listing
-Route::get('/listings/{listing}', function(Listing $listing){
-    
-   
-            return view('listing',[
-        'listing' => $listing
-    ]); 
-    
-
-});
+Route::get('/listings/{listing}', [ListingController::class,'show']);
 
 
 
