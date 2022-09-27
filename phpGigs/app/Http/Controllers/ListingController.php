cee8fefd3d9d336e20f51b458c238f2accb6ec9a
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-09-26 22:18:00 -0500

    corrected imageupload to database

---
+++ phpGigs/app/Http/Controllers/ListingController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Routing\Controller;
+
+class ListingController extends Controller
+{
+    //show all listings
+    public function index(){
+           
+return view('listings.index',[
+        
+        'listings' => Listing::latest()->filter(request(['tag','search']))->paginate(6)
+          ]);// ..or use ----   simplepaginate for ACK or NEXT button link at bottom
+    }
+
+    //showsingle listing
+    public function show(Listing $listing){
+       return view('listings.show',[
+        'listing' => $listing
+    ]); 
+    }
+
+
+    //show create form
+    public function create(){
+        return view('listings.create');
+    }
+
+
+    //store listing data
+    public function store(Request $request){
+
+
+        // dd($request->file('logo'));
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website'=> 'required',
+            'email' => ['required','email'],
+            'tags'=> 'required',
+            'description' => 'required'
+        ]);
+
+        if($request->hasFile('logo')){
+            $formFields['logo'] = $request->file('logo')->store('logos','public');
+        }
+        
+        Listing::create($formFields);
+        
+        
+        return redirect('/')->with('message','LISTING CREATED');
+
+    }
+
+}
---
+++ phpGigs/routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+//all listings
+Route::get('/', [ListingController::class,'index']);
+        
+  
+
+//show create form
+Route::get('/listings/create', [ListingController::class,'create']);
+
+
+          //store listing data
+Route::post('/listings', [ListingController::class,'store']);
+
+
+
+
+
+      //single listing
+Route::get('/listings/{listing}', [ListingController::class,'show']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/hello', function () {
+//     return 'hello world amd welcome home';
+// });
+
+
+// Route::get('/3rd', function () {
+//     return 'this is the 3rd page';
+// });
+
+
+// Route::get('/posts/{id}', function ($id) {
+// // ddd($id);
+
+//     return response('Post ' . $id)    ;
+// })-> where('id','[0-9]+');
+
+
+// Route::get('/search',function(Request $request){
+//     return $request->name . ' ' .$request->city;
+    
+// });
