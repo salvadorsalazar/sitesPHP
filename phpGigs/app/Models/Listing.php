8f6dff850a68d5c5d874ded541362aa207e83629
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-10-07 10:03:44 -0500

    added search param

--- phpGigs/app/Models/Listing.php
+++ phpGigs/app/Models/Listing.php
@@ -21,7 +21,12 @@ class Listing extends Model
              if($filters['search'] ?? false){
                         $query->where('title','like','%' . request('search' ). '%')
                         ->orWhere('description', 'like','%' . request('search'). '%')
-                         ->orWhere('tags', 'like','%' . request('search'). '%');
+                         ->orWhere('tags', 'like','%' . request('search'). '%')
+                          ->orWhere('company', 'like','%' . request('search'). '%')
+                         ->orWhere('location', 'like','%' . request('search'). '%')
+                                                 ->orWhere('email', 'like','%' . request('search'). '%')
+                                                                         ->orWhere('website', 'like','%' . request('search'). '%')
+                         ;
             }
 
 
