d8a9c1ef2df2a960d97c6e0c4a8452a3dafc7412
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-10-28 19:11:58 -0500

    updates

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,6 +1,8 @@
 <!-- <h1>Listings</h1>    ...-->
 <x-layout>
 {{-- @include('partials._hero') --}}
+
+{{--  --}}
 @include('partials._search')
 <div
                 class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4 bg-color:black "
